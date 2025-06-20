45903c5b27f066cf41a327de3e237bb9a0ee5bd6
xiaojintesha2019 <142777787+xiaojintesha2019@users.noreply.github.com>
2025-06-20 16:33:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ for bsz in ${bszs[*]}; do
 for len in ${lens[*]}; do
 for m in ${methods[*]}; do
 
-CUDA_VISIBLE_DEVICES=1  nohup python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data yb9webpagtest0 --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 3 --learning_rate 1e-3 --online_learning $online_learning --use_adbfgs >  pcpower$len$m$online_learning$m.out 2>&1 &
+CUDA_VISIBLE_DEVICES=1  nohup python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data PCpower --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 3 --learning_rate 1e-3 --online_learning $online_learning --use_adbfgs >  pcpower$len$m$online_learning$m.out 2>&1 &
 CUDA_VISIBLE_DEVICES=1  nohup python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data ETTh2 --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 3 --learning_rate 1e-3 --online_learning $online_learning --use_adbfgs >  ETTh2$len$m$online_learning.out 2>&1 &
 CUDA_VISIBLE_DEVICES=1  nohup python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data ETTm1 --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 3 --learning_rate 1e-3 --online_learning $online_learning --use_adbfgs >  ETTm1$len$m$online_learning.out 2>&1 &
 CUDA_VISIBLE_DEVICES=1  nohup python -u main.py  --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data WTH --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 3 --learning_rate 1e-3 --online_learning $online_learning --use_adbfgs >  WTH$len$m$online_learning.out 2>&1 &
